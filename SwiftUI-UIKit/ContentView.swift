853fc3f6894b577086b942e48deef2bf60a1b273
Daniel Ayala <imediayala@gmail.com>
2021-11-13 11:39:50 +0100

    Initial Commit

---
+++ SwiftUI-UIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-UIKit
+//
+//  Created by Daniel Ayala on 13/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-UIKit/SwiftUI_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIKitApp.swift
+//  SwiftUI-UIKit
+//
+//  Created by Daniel Ayala on 13/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
