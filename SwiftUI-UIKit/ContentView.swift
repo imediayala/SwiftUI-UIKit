94f41c1a2943cafbaf57e55be2b36ca2f665bdc0
Daniel Ayala <imediayala@gmail.com>
2021-11-13 12:18:13 +0100

    Implemented Coordinator

--- SwiftUI-UIKit/ContentView.swift
+++ SwiftUI-UIKit/ContentView.swift
@@ -9,29 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isShowingPhotoPicker = false
+    @State private var avatarImage = UIImage(named: "default-avatar")!
     
     var body: some View {
         VStack {
-            Image(uiImage: UIImage(named: "default-avatar")!)
+            Image(uiImage: avatarImage)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 150, height: 150)
                 .clipShape(Circle())
                 .padding()
-                .onTapGesture {
-                    
-                }
+                .onTapGesture { isShowingPhotoPicker = true }
+            
             Spacer()
         }
         .navigationTitle("Profile")
         .sheet(isPresented: $isShowingPhotoPicker, content: {
-            
+            PhotoPicker(avatarImage: $avatarImage)
         })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
